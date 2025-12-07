aa4ebcf2faea83fa9349ade98390e21d455fe712
sahanchamod12-beep <sahanchamod.12@gmail.com>
2025-12-07 16:07:11 +0530

    Update default SESSION_ID value in config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?Vug0w5'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?jPm8qQ'
 };
